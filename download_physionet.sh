949d34d7ce1e290006dc02bf60feb37cf82872d0
Ebrahim_Khan <44338681+ebrahim9214453@users.noreply.github.com>
2019-01-19 22:13:33 +0000

    Create download_physionet.sh

---
+++ download_physionet.sh
@@ -0,0 +1,6 @@
+wget https://physionet.org/pn4/sleep-edfx/ST7011J0-PSG.edf
+wget https://physionet.org/pn4/sleep-edfx/ST7011J0-PSG.edf.hyp
+wget https://physionet.org/pn4/sleep-edfx/ST7011JP-Hypnogram.edf
+wget https://physionet.org/pn4/sleep-edfx/ST7022J0-PSG.edf
+wget https://physionet.org/pn4/sleep-edfx/ST7022J0-PSG.edf.hyp
+wget https://physionet.org/pn4/sleep-edfx/ST7022JM-Hypnogram.edf
